bb7e41ac3449f9232aa029adb958da32203851cb
osuvileh <otso.suvilehto@gmail.com>
2022-05-04 22:25:18 +0300

    Copy more numerically stable vec_roll_to_mat3 implementation from https://coder-question.com/cq-blog/494393

--- mat_utils.py
+++ mat_utils.py
@@ -18,18 +18,50 @@ def round_matrix(mat, dec=17):
 
 
 def vec_roll_to_mat3(vec, roll):
-    target = Vector((0, 1, 0))
+    #Direct copy from https://coder-question.com/cq-blog/494393 
+    #port of the updated C function from armature.c
+    #https://developer.blender.org/T39470
+    #note that C accesses columns first, so all matrix indices are swapped compared to the C version
+
     nor = vec.normalized()
-    axis = target.cross(nor)
-    if axis.dot(axis) > 0.000001:
-        axis.normalize()
-        theta = target.angle(nor)
-        b_matrix = Matrix.Rotation(theta, 3, axis)
+    THETA_THRESHOLD_NEGY = 1.0e-9
+    THETA_THRESHOLD_NEGY_CLOSE = 1.0e-5
+
+    #create a 3x3 matrix
+    bMatrix = Matrix().to_3x3()
+
+    theta = 1.0 + nor[1]
+
+    if (theta > THETA_THRESHOLD_NEGY_CLOSE) or ((nor[0] or nor[2]) and theta > THETA_THRESHOLD_NEGY):
+
+        bMatrix[1][0] = -nor[0]
+        bMatrix[0][1] = nor[0]
+        bMatrix[1][1] = nor[1]
+        bMatrix[2][1] = nor[2]
+        bMatrix[1][2] = -nor[2]
+        if theta > THETA_THRESHOLD_NEGY_CLOSE:
+            #If nor is far enough from -Y, apply the general case.
+            bMatrix[0][0] = 1 - nor[0] * nor[0] / theta
+            bMatrix[2][2] = 1 - nor[2] * nor[2] / theta
+            bMatrix[0][2] = bMatrix[2][0] = -nor[0] * nor[2] / theta
+
+        else:
+            #If nor is too close to -Y, apply the special case.
+            theta = nor[0] * nor[0] + nor[2] * nor[2]
+            bMatrix[0][0] = (nor[0] + nor[2]) * (nor[0] - nor[2]) / -theta
+            bMatrix[2][2] = -bMatrix[0][0]
+            bMatrix[0][2] = bMatrix[2][0] = 2.0 * nor[0] * nor[2] / theta
+
     else:
-        updown = 1 if target.dot(nor) > 0 else -1
-        b_matrix = Matrix.Scale(updown, 3)
-    r_matrix = Matrix.Rotation(roll, 3, nor)
-    mat = r_matrix @ b_matrix
+        #If nor is -Y, simple symmetry by Z axis.
+        bMatrix = Matrix().to_3x3()
+        bMatrix[0][0] = bMatrix[1][1] = -1.0
+
+    #Make Roll matrix
+    rMatrix = Matrix.Rotation(roll, 3, nor)
+
+    #Combine and output result
+    mat = rMatrix @ bMatrix
     return mat
 
 
